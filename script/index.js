71f0e3c4db94b00cd208f6baa72d65e5329c4e99
Bigyan Kumar Sandilya <bigyankumarnwd@gmail.com>
2022-02-22 01:14:35 +0530

    slide part has done
    poster slide has been done

---
+++ script/index.js
@@ -0,0 +1,24 @@
+document.querySelector(".fa-angle-down").addEventListener("click",popbox)
+function popbox(){
+    Box = document.getElementById("box")
+    Box.style.display = "block"
+}
+
+
+
+//  auto slide 
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("slide")
+  bullet = document.getElementById("bullet-slide")
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none"; 
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 2000); // Change image every 12 seconds
+}
