3f2b94ccc16f3ed5be9b91e4ffcc337e61f26db0
Bigyan Kumar Sandilya <bigyankumarnwd@gmail.com>
2022-02-22 16:24:06 +0530

    re bhai koi slide kr do bhai

--- script/index.js
+++ script/index.js
@@ -27,3 +27,23 @@ function showSlides() {
   slides[slideIndex-1].style.display = "block";
   setTimeout(showSlides, 2000); // Change image every 12 seconds
 }
+
+
+// slider part
+const productContainers = [...document.querySelectorAll('.product-container')];
+const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
+const preBtn = [...document.querySelectorAll('.pre-btn')];
+
+productContainers.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+
+    nxtBtn[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth;
+    })
+
+    preBtn[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth;
+    })
+})
+// slider end
