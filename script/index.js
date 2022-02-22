46310f9c790d57dbe8b1edf22ba7f1ccc44943de
Mohd-Haroon <99546040+Mohd-Haroon@users.noreply.github.com>
2022-02-22 19:13:04 +0530

    added partner slider
    a

--- script/index.js
+++ script/index.js
@@ -47,3 +47,20 @@ productContainers.forEach((item, i) => {
     })
 })
 // slider end
+// partnerslider
+const productContainers = [...document.querySelectorAll('.partner-container')];
+const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
+const preBtn = [...document.querySelectorAll('.pre-btn')];
+
+productContainers.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+
+    nxtBtn[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth;
+    })
+
+    preBtn[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth;
+    })
+})
