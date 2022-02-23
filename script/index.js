7d0f7b550cc363f70f7008b5cf0bf5d5a1499759
Mohd-Haroon <99546040+Mohd-Haroon@users.noreply.github.com>
2022-02-23 15:28:43 +0530

    some improvement
    slider partner

--- script/index.js
+++ script/index.js
@@ -30,24 +30,6 @@ function showSlides() {
 
 
 // slider part
-const productContainers = [...document.querySelectorAll('.product-container')];
-const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
-const preBtn = [...document.querySelectorAll('.pre-btn')];
-
-productContainers.forEach((item, i) => {
-    let containerDimensions = item.getBoundingClientRect();
-    let containerWidth = containerDimensions.width;
-
-    nxtBtn[i].addEventListener('click', () => {
-        item.scrollLeft += containerWidth;
-    })
-
-    preBtn[i].addEventListener('click', () => {
-        item.scrollLeft -= containerWidth;
-    })
-})
-// slider end
-// partnerslider
 const productContainers = [...document.querySelectorAll('.partner-container')];
 const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
 const preBtn = [...document.querySelectorAll('.pre-btn')];
@@ -64,3 +46,21 @@ productContainers.forEach((item, i) => {
         item.scrollLeft -= containerWidth;
     })
 })
+
+const productContainers1 = [...document.querySelectorAll('.product-container')];
+const nxtBtn1 = [...document.querySelectorAll('.nxt-btn')];
+const preBtn1 = [...document.querySelectorAll('.pre-btn')];
+
+productContainers1.forEach((item, i) => {
+    let containerDimensions1 = item.getBoundingClientRect();
+    let containerWidth1 = containerDimensions1.width;
+
+    nxtBtn1[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth1;
+    })
+
+    preBtn1[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth1;
+    })
+})
+
