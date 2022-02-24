224522af00281d7f959ed3d964e7da0568f44a3c
Bigyan Kumar Sandilya <bigyankumarnwd@gmail.com>
2022-02-24 15:12:02 +0530

    deta base is ready so plz play with

---
+++ product/fetchdeta.js
@@ -0,0 +1,43 @@
+
+document.querySelector("#covid").addEventListener("click", covid)
+document.querySelector("#diabetes").addEventListener("click",diabetes)
+document.querySelector("#eyewear").addEventListener("click",eyewear)
+document.querySelector("#ayush").addEventListener("click",ayush)
+document.querySelector("#fitness").addEventListener("click",fitness)
+document.querySelector("#mom").addEventListener("click",mom)
+document.querySelector("#devices").addEventListener("click",devices)
+document.querySelector("#surgicals").addEventListener("click",surgicals)
+document.querySelector("#sexual").addEventListener("click",sexual)
+document.querySelector("#treatments").addEventListener("click",treatments)
+
+
+function covid(){
+console.log("covid")
+}
+function diabetes(){
+    console.log("diabetes")
+}
+function eyewear(){
+  console.log("eyewear")  
+}
+function ayush(){
+    console.log("ayush")  
+  }
+  function fitness(){
+    console.log("fitness")  
+  }
+  function mom(){
+    console.log("mom")  
+  }
+  function surgicals(){
+    console.log("surgicals")  
+  }
+  function sexual(){
+    console.log("sexual")  
+  }
+  function treatments(){
+    console.log("treatments")  
+  }
+  function devices(){
+    console.log("devices")  
+  }
