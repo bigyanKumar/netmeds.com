cc42a71b0a242b55fd4f9f3de8a0b83887bc23be
Bigyan Kumar Sandilya <bigyankumarnwd@gmail.com>
2022-02-25 10:29:10 +0530

    product

--- product/product.js
+++ product/product.js
@@ -1,7 +1,9 @@
 tempDeta = JSON.parse(localStorage.getItem("TempProductItem"))||[]
-    document.querySelector("#productItem").innerText = ""
-
-
+    document.querySelector("#outof").innerText = tempDeta.length-1
+console.log(tempDeta.length-1)
+showDeta()
+function showDeta(){
+    document.querySelector("#productItem").innerHTML = ""
     tempDeta.map(function(event){
 
         div1 = document.createElement("div")
@@ -14,7 +16,7 @@ tempDeta = JSON.parse(localStorage.getItem("TempProductItem"))||[]
         p2 = document.createElement("p")
         p2.innerText = "Mkt :"+ event.mkt
         p3 = document.createElement("p")
-        p3.innerText ="Best Price: "+(event.aprice*event.dis/100)
+        p3.innerText ="Best Price: "+(event.aprice-(event.aprice*event.dis/100))
         p4 = document.createElement("p")
         p4.innerText = "MRP Rs: "+event.aprice
         button = document.createElement("button")
@@ -23,3 +25,10 @@ tempDeta = JSON.parse(localStorage.getItem("TempProductItem"))||[]
         console.log(div1)
         document.querySelector("#productItem").append(div1)
     })
+}
+
+
+    
+    alldeta = tempDeta = JSON.parse(localStorage.getItem("productDetaBase"))||[]
+
+    document.querySelector("#total").innerText = alldeta.length-1
